b6a87f0dbf0eed2d528648a26799f75c605700d7
asd3638 <asd36384@gmail.com>
2022-02-05 08:30:41 +0900

    [FEAR]: delete cafe

--- server/src/main/java/com/sook4/beanYard/api/service/apply/ApplyService.java
+++ server/src/main/java/com/sook4/beanYard/api/service/apply/ApplyService.java
@@ -46,6 +46,8 @@ public class ApplyService {
         user.setCoffee(user.getCoffee() + byId.getCoffee());
         userRepository.saveAndFlush(user);
 
+        cafeRepository.delete(byId);
+
         return Optional.of(modelMapper.map(savedApply, ApplyDto.class));
     }
 
