f7cf4067c9e82f6ed6c6b713d5f80ee5dbe7e810
minsun <meeeeensun@gmail.com>
2022-02-05 07:10:02 +0900

    주석 삭제

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -4,61 +4,6 @@ import { getApi } from "../api";
 import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
-    // const [loginErrorMsg, setLoginErrorMsg] = useState("");
-    // const submitHandler = async (e) => {
-    //     e.preventDefault();
-
-    //     const post = async () => {
-    //         try {
-    //             const res = await axios.post("http://27.96.134.100:8080/login",
-    //                 userData, {
-    //                     headers: {
-    //                         'Content-Type': 'application/json',
-    //                         Accept: "application/json",
-    //                     }
-    //                 }
-    //             )
-    //             // console.log(res);
-    //             // console.log(res.headers.authorization);
-    //             await getApi(
-    //                 {
-    //                     userName: userData.userName,
-    //                 },
-    //                 "/api/user"
-    //             )
-    //             .then(({ status, data }) => {
-    //                 if (status === 200) {
-    //                     // console.log('getdata', data);
-    //                     setAuth({ 
-    //                         ...auth, 
-    //                         type: "login",
-    //                         token: res.headers.authorization,
-    //                         userName: data.userName,
-    //                         userType: data.userType,
-    //                         userSeq: data.userSeq,
-    //                     })
-    //                 } else if (status === 401) {
-    //                     setLoginErrorMsg("Try Again!");
-    //                 }
-    //             })
-    //             .catch((e) => {
-    //                 setLoginErrorMsg("Try Again!");
-    //                 console.log(e);
-    //             });
-
-    //             authContext.dispatch({
-    //                 ...auth
-    //             });
-    //             if (auth.token !== "") {
-    //                 navigate("/"); // 로그인 성공 시 Home으로 이동
-    //             }
-    //             // console.log(authContext.state);
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    //     await post();
-    // };
     const authContext = useContext(AuthContext);
     const navigate = useNavigate();
 
