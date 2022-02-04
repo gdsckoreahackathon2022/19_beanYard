b711d23381948adc9414f3edeca5208e5051d36a
minsun <meeeeensun@gmail.com>
2022-02-05 08:06:26 +0900

    로그아웃에 로컬스토리지도 제거

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -22,6 +22,7 @@ const Logout = () => {
                         userSeq: null,
                     });
                     alert('로그아웃 되었습니다.');
+                    localStorage.clear();
                 } else {
                     alert('로그아웃에 실패했습니다. 다시 시도해주세요.');
                 }
